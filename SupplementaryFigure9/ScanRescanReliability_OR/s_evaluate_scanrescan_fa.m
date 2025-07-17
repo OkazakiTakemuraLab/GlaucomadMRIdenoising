41b4e9b6005673a809fd216e47ffb54b13d79c5a
Daiki Taguma <dtaguma@nips.ac.jp>
2025-07-17 16:45:02 +0900

    Update s_evaluate_scanrescan_fa.m
    Update the article information

--- SupplementaryFigure9/ScanRescanReliability_OR/s_evaluate_scanrescan_fa.m
+++ SupplementaryFigure9/ScanRescanReliability_OR/s_evaluate_scanrescan_fa.m
@@ -2,8 +2,8 @@ function s_evaluate_scanrescan_fa
 
 % This script aims to replicate top panels in Supplementaly Figure 9 of the following
 % article:
-% Taguma, D., Ogawa, S. & Takemura, H. (in revision) Evaluating the impact of
-% denoising diffusion MRI data on tractometry metrics of optic tract abnormalities in glaucoma.
+% Taguma, D., Ogawa, S. & Takemura, H. (2025) Evaluating the impact of denoising diffusion MRI data
+% on tractometry metrics of optic tract abnormalities in glaucoma. Scientific Reports, 15, 25812.
 %
 % Daiki Taguma, NIPS SCBM/SOKENDAI
 
